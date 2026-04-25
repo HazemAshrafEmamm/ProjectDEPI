98839c19fe8fb13d55ab8b589bfaedf338f194e0
Yuossef-ezzat <yuossefezzatmostafa@gmail.com>
2026-04-25 14:55:58 +0300

    Stracture of the project

---
+++ DAL/Data/TabibyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public class TabibyDbContext : DbContext
+    {
+        public TabibyDbContext(DbContextOptions<TabibyDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ PL/Program.cs
@@ -0,0 +1,48 @@
+using DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace PL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+
+
+            //DbContext
+            builder.Services.AddDbContext<TabibyDbContext>(
+                options => {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("InventoryDbContext"));
+                }
+            );
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
