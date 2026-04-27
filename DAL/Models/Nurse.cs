3d259fd4c25189e69a1fc2fec10e696d795fddd2
minageorge-77 <minageorge909@gmail.com>
2026-04-27 22:21:57 +0300

    Add Pharmacist and Nurse models

---
+++ DAL/Models/Nurse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Identity.Client;
+
+namespace DAL.Models
+{
+    public class Nurse
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; } = null!;
+
+        public string Specialization { get; set; } = string.Empty;
+
+        public virtual ICollection<NursingRequest> NursingRequests { get; set; } = new List<NursingRequest>();
+    }
+}
---
+++ DAL/Models/Pharmacist .cs
@@ -0,0 +1,26 @@
+﻿//using Castle.Components.DictionaryAdapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Identity.Client;
+
+
+namespace DAL.Models
+{
+    public class Pharmacist
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; } = null!;
+
+        public string PharmacyName { get; set; } = string.Empty;
+
+        public virtual ICollection<Medication> Medications { get; set; } = new List<Medication>();
+    }
+}
